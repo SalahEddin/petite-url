0b87740950963fb5127a2251063a205db582917b
Salah Eddin Alshaal <salshaal@gmail.com>
2020-10-11 18:42:50 +0200

    feat(url): Shorten URL method

--- src/url_utils.py
+++ src/url_utils.py
@@ -1,14 +1,31 @@
 import re
 
+
 def encode_url(url):
     return url
 
 
 def validate_url(url):
     regex = re.compile(
-        r'^(?:http|ftp)s?://'
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' # Domain
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # or IPv4
-        r'(?::\d+)?' # optional port
-        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+        r"^(?:http|ftp)s?://"
+        r"(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|"  # Domain
+        r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})"  # or IPv4
+        r"(?::\d+)?"  # optional port
+        r"(?:/?|[/?]\S+)$",
+        re.IGNORECASE,
+    )
     return re.match(regex, url) is not None
+
+
+def shorten_url(get_counter_value, unwrapped_url):
+    # base 62 characters
+    characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    base = len(characters)
+    counter = get_counter_value()
+    encoded = []
+    while counter > 0:
+        val = counter % base
+        encoded.append(characters[val])
+        counter = counter // base  # double slash returns the integer in python3
+    # since `encoded` has reversed order of base62 id, reverse ret before return it
+    return "".join(encoded[::-1])
--- test_sample.py
+++ test_sample.py
@@ -1,19 +1,35 @@
 import pytest
-from src.url_utils import validate_url
+from src.url_utils import validate_url, shorten_url
 
 
 @pytest.mark.parametrize(
     "url,expected",
     [
-		("dummy_string", False),
+        ("dummy_string", False),
         ("https://www.facebook.com/", True),
         ("http://www.facebook.com/", True),
         ("https://www.facebook.com", True),
         ("https://www.facebook💤.com", False),
         ("https://www.facebook .com", False),
-		("ht://www.facebook .com", False),
-		("http://www.facebook.com:8080", True),
+        ("ht://www.facebook .com", False),
+        ("http://www.facebook.com:8080", True),
     ],
 )
-def test_answer(url, expected):
+def test_url_validation(url, expected):
     assert validate_url(url) == expected
+
+
+@pytest.mark.parametrize(
+    "unwrapped_url,id,expected",
+    [
+        ("https://www.facebook.com/", 1, "1"),
+        ("https://www.facebook.com/", 63, "11"),
+        ("https://www.facebook.com/", 9000, "2la"),
+		("https://www.facebook.com/", 14776336, "10000"),
+		("https://www.facebook.com/", 14776335, "ZZZZ"),
+    ],
+)
+def test_url_shortnening(unwrapped_url, id, expected):
+    getCounter = lambda: id
+
+    assert shorten_url(getCounter, unwrapped_url) == expected
